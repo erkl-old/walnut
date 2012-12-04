4c1872010f3e7a3adddd8bd15f18d84d75321e9c
Erik Lundin <erik@wub.io>
2012-12-04 21:49:18 +0000

    Start fresh on type parsing

--- types.go
+++ types.go
@@ -1,132 +1,18 @@
 package walnut
 
 import (
-	"strconv"
+	"strings"
 	"time"
 )
 
-const (
-	TypeString = iota // = 0
-	TypeInt
-	TypeBool
-	TypeDuration
-	TypeTime
-
-	TypeNone = -1
-)
-
-func DetectType(input string) int {
-	if _, ok := ParseString(input); ok {
-		return TypeString
-	}
-
-	if _, ok := ParseInt(input); ok {
-		return TypeInt
-	}
-
-	if _, ok := ParseBool(input); ok {
-		return TypeBool
-	}
-
-	if _, ok := ParseDuration(input); ok {
-		return TypeDuration
-	}
-
-	if _, ok := ParseTime(input); ok {
-		return TypeTime
-	}
-
-	return TypeNone
-}
-
-func ParseString(input string) (string, bool) {
-	if input == "" || input[0] != '"' {
-		return "", false
-	}
-
-	value, err := strconv.Unquote(input)
-	if err != nil {
-		return "", false
-	}
-
-	return value, true
-}
-
-func ParseInt(input string) (int64, bool) {
-	value, err := strconv.ParseInt(input, 0, 64)
-	if err != nil {
-		return 0, false
-	}
-
-	return value, true
-}
-
-func ParseBool(input string) (bool, bool) {
-	switch input {
-	case "true", "yes", "on":
-		return true, true
-	case "false", "no", "off":
-		return false, true
-	}
-
-	return false, false
-}
-
-func ParseDuration(input string) (time.Duration, bool) {
-	// don't bother parsing an empty string
-	if input == "" {
-		return 0, false
-	}
-
-	total := time.Duration(0)
-	var lastUnit time.Duration
-
-	for {
-		// consume leading whitespace
-		for input != "" && input[0] == ' ' {
-			input = input[1:]
-		}
-
-		// stop with nothing left to parse
-		if input == "" {
-			break
-		}
-
-		// parse the number and the unit; fail if either of them
-		// are missing
-		num, digits := readDigits(input)
-		unit, chars := readUnit(input[digits:])
-
-		if digits == 0 || chars == 0 {
-			return 0, false
-		}
-
-		// make sure the components are ordered by unit, in
-		// descending order
-		if unit >= lastUnit && lastUnit != 0 {
-			return 0, false
-		}
-
-		// detect integer overflows
-		next := total + (time.Duration(num) * unit)
-		if next < total {
-			return 0, false
-		}
-
-		lastUnit = unit
-		input = input[digits+chars:]
-
-		total = next
-	}
-
-	return total, true
-}
-
-func readDigits(s string) (int64, int) {
+// Reads ASCII digits from the beginning of a string until a) the string
+// ends, a non-digit character is encountered, or when an integer overflow
+// occurs. Also returns the number of characters consumed if successful.
+func digits(input string) (int64, int) {
 	total := int64(0)
 
-	for i := 0; i < len(s); i++ {
-		char := s[i]
+	for i := 0; i < len(input); i++ {
+		char := input[i]
 
 		// stop on the first non-digit character
 		if char < '0' || char > '9' {
@@ -147,40 +33,31 @@ func readDigits(s string) (int64, int) {
 	return 0, 0
 }
 
-func readUnit(s string) (time.Duration, int) {
+// Reads a unit of duration from the beginning of a string. Also returns
+// the number of bytes read if successful.
+func unit(s string) (time.Duration, int) {
 	switch {
-	case hasPrefix(s, "ns"):
+	case strings.HasPrefix(s, "ns"):
 		return time.Nanosecond, 2
-	case hasPrefix(s, "μs"), hasPrefix(s, "µs"):
+	case strings.HasPrefix(s, "μs"):
 		return time.Microsecond, 3
-	case hasPrefix(s, "us"):
+	case strings.HasPrefix(s, "µs"):
 		return time.Microsecond, 2
-	case hasPrefix(s, "ms"):
+	case strings.HasPrefix(s, "us"):
+		return time.Microsecond, 2
+	case strings.HasPrefix(s, "ms"):
 		return time.Millisecond, 2
-	case hasPrefix(s, "s"):
+	case strings.HasPrefix(s, "s"):
 		return time.Second, 1
-	case hasPrefix(s, "m"):
+	case strings.HasPrefix(s, "m"):
 		return time.Minute, 1
-	case hasPrefix(s, "h"):
+	case strings.HasPrefix(s, "h"):
 		return time.Hour, 1
-	case hasPrefix(s, "d"):
+	case strings.HasPrefix(s, "d"):
 		return 24 * time.Hour, 1
-	case hasPrefix(s, "w"):
+	case strings.HasPrefix(s, "w"):
 		return 7 * 24 * time.Hour, 1
 	}
 
 	return 0, 0
-}
-
-func hasPrefix(s, prefix string) bool {
-	return len(s) >= len(prefix) && s[0:len(prefix)] == prefix
-}
-
-func ParseTime(input string) (time.Time, bool) {
-	t, err := time.Parse("2006-01-02 15:04:05 -0700", input)
-	if err != nil {
-		return time.Time{}, false
-	}
-
-	return t, true
 }
