67b6ab3a663482c979423200f18db78239229c0c
Erik Lundin <erik@wub.io>
2012-12-05 14:05:55 +0000

    Implement and test ParseFloat

--- types.go
+++ types.go
@@ -1,11 +1,50 @@
 package walnut
 
 import (
+	"strconv"
 	"strings"
 	"time"
 )
 
-// Reads ASCII digits from the beginning of a string until the string
+// Attempts to parse the input string as a floating point (or integral)
+// number.
+func ParseFloat(input string) (float64, bool) {
+	digits := 0
+	dot := -1
+
+	for i, r := range input {
+		switch {
+		case (r == '-' || r == '+') && i == 0:
+			// ignore signs at the beginning of the string
+		case r >= '0' && r <= '9':
+			digits++
+		// only allow a dot if we haven't encountered one before and
+		// it's preceeded by at least one digit
+		case r == '.' && dot == -1 && digits > 0:
+			dot = digits
+		default:
+			// any other circumstance should result in a parsing error
+			return 0, false
+		}
+	}
+
+	// make sure we have encountered at least one digit, and that there
+	// are digits after the dot (if it is present)
+	if digits == 0 || dot >= digits {
+		return 0, false
+	}
+
+	// floating point values are fickle things best left to be dealt with
+	// by cleverer people; let strconv do the heavy lifting for us
+	value, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, false
+	}
+
+	return value, true
+}
+
+// Reads ASCII digits from the beginning of a string until either the string
 // ends, a non-digit character is encountered, or when an integer overflow
 // occurs. Also returns the number of characters consumed if successful.
 func digits(input string) (int64, int) {
---
+++ types_test.go
@@ -0,0 +1,62 @@
+package walnut
+
+import (
+	"testing"
+)
+
+// test suite for ParseFloat
+var floats = []struct {
+	in string
+	f  float64
+	ok bool
+}{
+	// integers
+	{"1", 1, true},
+	{"987654321", 987654321, true},
+	{"123456700", 1.234567e+08, true},
+
+	// floats
+	{"1.3", 1.3, true},
+	{"100.0", 100, true},
+	{"38.002", 38.002, true},
+	{"0.1", 0.1, true},
+
+	// signs
+	{"-0", 0, true},
+	{"+1", 1, true},
+	{"-2", -2, true},
+	{"-0.1", -0.1, true},
+	{"-3.4", -3.4, true},
+
+	// long
+	{"100000000000000000000000", 1e+23, true},
+	{"99999999999999974834176", 9.999999999999997e+22, true},
+	{"100000000000000000000001", 1.0000000000000001e+23, true},
+	{"100000000000000008388608", 1.0000000000000001e+23, true},
+	{"100000000000000016777215", 1.0000000000000001e+23, true},
+	{"100000000000000016777216", 1.0000000000000003e+23, true},
+	{"22.222222222222222", 22.22222222222222, true},
+
+	// invalid
+	{"", 0, false},
+	{"1a", 0, false},
+	{"0x30", 0, false},
+	{"1.1.", 0, false},
+	{"+-0", 0, false},
+	{"-0-", 0, false},
+}
+
+func TestParseFloat(test *testing.T) {
+	h := "ParseFloat(%#v) ->"
+
+	for _, t := range floats {
+		f, ok := ParseFloat(t.in)
+
+		switch {
+		case ok != t.ok:
+			test.Errorf(h+" ok != %#v", t.in, t.ok)
+		case f != t.f:
+			test.Errorf(h+" %#v, want %#v", t.in, f, t.f)
+		}
+	}
+}
