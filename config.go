0f64e842cb967901cb3bb0b29417509ebb2978da
Erik Lundin <erik@wub.io>
2013-01-15 13:09:00 +0000

    Remove Config interface, define a struct instead

--- config.go
+++ config.go
@@ -4,26 +4,56 @@ import (
 	"time"
 )
 
-type Config interface {
-	// Returns a list of all keys, sorted lexographically.
-	Keys() []string
+type Config map[string]string
 
-	// Returns a new Config instance using a subset of the current
-	// configuration, selecting all keys with the supplied prefix.
-	Prefix(prefix string) Config
+// Returns a list of all defined keys, sorted lexographically.
+func (c *Config) Keys() []string {
+	return make([]string, 0)
+}
 
-	// Loads values from another config object.
-	Extend(other Config) Config
+// Creates a new config instance containing the keys in `.
+func (c *Config) Select(prefix string) *Config {
+	return c
+}
 
-	// Assigns a value to a key. Panics if the value is not of a legal type.
-	Set(key string, value interface{})
+// Retrieves a value. Will return a non-nil error if the key either
+// hasn't been defined or is of a different type.
+func (c *Config) Get(key string) (interface{}, error) {
+	return nil, nil
+}
 
-	// Returns the key's value, or an error if the key is undefined.
-	Get(key string) (interface{}, error)
+// Retrieves a string value. Will return a non-nil error if the key
+// either hasn't been defined or is of a different type.
+func (c *Config) String(key string) (string, error) {
+	return "", nil
+}
 
-	String(key string) (string, error)
-	Bool(key string) (bool, error)
-	Float(key string) (float64, error)
-	Duration(key string) (time.Duration, error)
-	Time(key string) (time.Time, error)
+// Retrieves a bool value. Will return a non-nil error if the key
+// either hasn't been defined or is of a different type.
+func (c *Config) Bool(key string) (bool, error) {
+	return false, nil
+}
+
+// Retrieves an integer value. Will return a non-nil error if the key
+// either hasn't been defined or is of a different type.
+func (c *Config) Int64(key string) (int64, error) {
+	return 0, nil
+}
+
+// Retrieves a float value. Will return a non-nil error if the key
+// either hasn't been defined or is of a different type.
+func (c *Config) Float64(key string) (float64, error) {
+	return 0, nil
+}
+
+// Retrieves a duration value. Will return a non-nil error if the key
+// either hasn't been defined or is of a different type.
+func (c *Config) Duration(key string) (time.Duration, error) {
+	return time.Duration(0), nil
+}
+
+// Retrieves a time value. Will return a non-nil error if the key
+// either hasn't been defined or is of a different type.
+func (c *Config) Time(key string) (time.Time, error) {
+	return time.Now(), nil
 }
