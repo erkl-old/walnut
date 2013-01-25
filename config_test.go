a0f6014b70916ad306b6f6937169c8f77eec7621
Erik Lundin <erik@wub.io>
2013-01-25 19:25:08 +0000

    Make Config accessor tests more thorough
    "Finally, more arrays of anonymous structs!"

--- config_test.go
+++ config_test.go
@@ -6,25 +6,23 @@ import (
 )
 
 var sample = Config{
-	"http.host":       "0.0.0.0",
-	"http.port":       int64(8080),
-	"greeting.string": "hello",
-	"greeting.delay":  2 * time.Second,
-	"cake-ratio":      float64(1.0),
-	"timestamp":       time.Date(2012, 12, 28, 15, 10, 15, 0, time.UTC),
-	"debug-mode":      true,
+	"string":   "hello",
+	"bool":     true,
+	"int64":    int64(12345),
+	"float64":  float64(123.45),
+	"time":     time.Date(2012, 12, 28, 15, 10, 15, 0, time.UTC),
+	"duration": 2 * time.Second,
 }
 
 func TestConfigKeys(t *testing.T) {
 	actual := sample.Keys()
 	expected := []string{
-		"cake-ratio",
-		"debug-mode",
-		"greeting.delay",
-		"greeting.string",
-		"http.host",
-		"http.port",
-		"timestamp",
+		"bool",
+		"duration",
+		"float64",
+		"int64",
+		"string",
+		"time",
 	}
 
 	if len(actual) != len(expected) {
@@ -39,139 +37,161 @@ func TestConfigKeys(t *testing.T) {
 }
 
 func TestConfigGet(t *testing.T) {
+	// @todo: .(map[string]interface{})
+
 	v, ok := sample.Get("undefined")
 	if v != nil || ok != false {
 		t.Fatalf("Config.Get(%q) -> %v, %v (want %v, %v)",
 			"undefined", v, ok, nil, false)
 	}
 
-	v, ok = sample.Get("cake-ratio")
-	if v.(float64) != 1.0 || ok != true {
-		t.Fatalf("Config.Get(%q) -> %v, %v (want %v, %v)",
-			"cake-ratio", v, ok, float64(1.0), true)
+	v, ok = sample.Get("float64")
+	if v.(float64) != 123.45 || ok != true {
+		t.Fatalf("Config.Get(%q) -> %#v, %#v (want %#v, %#v)",
+			"float64", v, ok, float64(1.0), true)
 	}
+}
+
+var stringTests = []struct {
+	key   string
+	value string
+	err   error
+}{
+	{"undefined", "", ErrUndefined},
+	{"string", "hello", nil},
+	{"bool", "", ErrWrongType},
+	{"int64", "", ErrWrongType},
+	{"float64", "", ErrWrongType},
+	{"time", "", ErrWrongType},
+	{"duration", "", ErrWrongType},
 }
 
 func TestConfigString(t *testing.T) {
-	v, err := sample.String("undefined")
-	if v != "" || err != ErrUndefined {
-		t.Fatalf("Config.String(%q) -> %#v, %#v (want %#v, %#v)",
-			"undefined", v, err, "", ErrUndefined)
+	for _, test := range stringTests {
+		v, err := sample.String(test.key)
+		if v != test.value || err != test.err {
+			t.Errorf("Config.String(%q) -> %#v, %#v (want %#v, %#v)",
+				test.key, v, err, test.value, test.err)
+		}
 	}
+}
 
-	v, err = sample.String("greeting.delay")
-	if v != "" || err != ErrWrongType {
-		t.Fatalf("Config.String(%q) -> %#v, %#v (want %#v, %#v)",
-			"greeting.delay", v, err, "", ErrWrongType)
-	}
-
-	v, err = sample.String("greeting.string")
-	if v != "hello" || err != nil {
-		t.Fatalf("Config.String(%q) -> %#v, %#v (want %#v, %#v)",
-			"greeting.string", v, err, "hello", nil)
-	}
+var boolTests = []struct {
+	key   string
+	value bool
+	err   error
+}{
+	{"undefined", false, ErrUndefined},
+	{"string", false, ErrWrongType},
+	{"bool", true, nil},
+	{"int64", false, ErrWrongType},
+	{"float64", false, ErrWrongType},
+	{"time", false, ErrWrongType},
+	{"duration", false, ErrWrongType},
 }
 
 func TestConfigBool(t *testing.T) {
-	v, err := sample.Bool("undefined")
-	if v != false || err != ErrUndefined {
-		t.Fatalf("Config.Bool(%q) -> %v, %#v (want %#v, %#v)",
-			"undefined", v, err, false, ErrUndefined)
+	for _, test := range boolTests {
+		v, err := sample.Bool(test.key)
+		if v != test.value || err != test.err {
+			t.Errorf("Config.Bool(%q) -> %#v, %#v (want %#v, %#v)",
+				test.key, v, err, test.value, test.err)
+		}
 	}
+}
 
-	v, err = sample.Bool("cake-ratio")
-	if v != false || err != ErrWrongType {
-		t.Fatalf("Config.Bool(%q) -> %#v, %#v (want %#v, %#v)",
-			"cake-ratio", v, err, false, ErrWrongType)
-	}
-
-	v, err = sample.Bool("debug-mode")
-	if v != true || err != nil {
-		t.Fatalf("Config.Bool(%q) -> %#v, %#v (want %#v, %#v)",
-			"debug-mode", v, err, true, nil)
-	}
+var int64Tests = []struct {
+	key   string
+	value int64
+	err   error
+}{
+	{"undefined", 0, ErrUndefined},
+	{"string", 0, ErrWrongType},
+	{"bool", 0, ErrWrongType},
+	{"int64", 12345, nil},
+	{"float64", 0, ErrWrongType},
+	{"time", 0, ErrWrongType},
+	{"duration", 0, ErrWrongType},
 }
 
 func TestConfigInt64(t *testing.T) {
-	v, err := sample.Int64("undefined")
-	if v != 0 || err != ErrUndefined {
-		t.Fatalf("Config.Int64(%q) -> %#v, %#v (want %#v, %#v)",
-			"undefined", v, err, 0, ErrUndefined)
+	for _, test := range int64Tests {
+		v, err := sample.Int64(test.key)
+		if v != test.value || err != test.err {
+			t.Errorf("Config.Int64(%q) -> %#v, %#v (want %#v, %#v)",
+				test.key, v, err, test.value, test.err)
+		}
 	}
+}
 
-	v, err = sample.Int64("greeting.delay")
-	if v != 0 || err != ErrWrongType {
-		t.Fatalf("Config.Int64(%q) -> %#v, %#v (want %#v, %#v)",
-			"greeting.delay", v, err, 0, ErrWrongType)
-	}
-
-	v, err = sample.Int64("http.port")
-	if v != 8080 || err != nil {
-		t.Fatalf("Config.Int64(%q) -> %#v, %#v (want %#v, %#v)",
-			"http.port", v, err, 8080, nil)
-	}
+var float64Tests = []struct {
+	key   string
+	value float64
+	err   error
+}{
+	{"undefined", 0, ErrUndefined},
+	{"string", 0, ErrWrongType},
+	{"bool", 0, ErrWrongType},
+	{"int64", 0, ErrWrongType},
+	{"float64", 123.45, nil},
+	{"time", 0, ErrWrongType},
+	{"duration", 0, ErrWrongType},
 }
 
 func TestConfigFloat64(t *testing.T) {
-	v, err := sample.Float64("undefined")
-	if v != 0 || err != ErrUndefined {
-		t.Fatalf("Config.Float64(%q) -> %#v, %#v (want %#v, %#v)",
-			"undefined", v, err, 0, ErrUndefined)
+	for _, test := range float64Tests {
+		v, err := sample.Float64(test.key)
+		if v != test.value || err != test.err {
+			t.Errorf("Config.Float64(%q) -> %#v, %#v (want %#v, %#v)",
+				test.key, v, err, test.value, test.err)
+		}
 	}
+}
 
-	v, err = sample.Float64("greeting.delay")
-	if v != 0 || err != ErrWrongType {
-		t.Fatalf("Config.Float64(%q) -> %#v, %#v (want %#v, %#v)",
-			"greeting.delay", v, err, 0, ErrWrongType)
-	}
-
-	v, err = sample.Float64("cake-ratio")
-	if v != 1.0 || err != nil {
-		t.Fatalf("Config.Float64(%q) -> %#v, %#v (want %#v, %#v)",
-			"cake-ratio", v, err, 8080, nil)
-	}
+var timeTests = []struct {
+	key   string
+	value time.Time
+	err   error
+}{
+	{"undefined", time.Time{}, ErrUndefined},
+	{"string", time.Time{}, ErrWrongType},
+	{"bool", time.Time{}, ErrWrongType},
+	{"int64", time.Time{}, ErrWrongType},
+	{"float64", time.Time{}, ErrWrongType},
+	{"time", time.Date(2012, 12, 28, 15, 10, 15, 0, time.UTC), nil},
+	{"duration", time.Time{}, ErrWrongType},
 }
 
 func TestConfigTime(t *testing.T) {
-	zero := time.Time{}
-
-	v, err := sample.Time("undefined")
-	if v != zero || err != ErrUndefined {
-		t.Fatalf("Config.Time(%q) -> %s, %#v (want %s, %#v)",
-			"undefined", v, err, 0, ErrUndefined)
-	}
-
-	v, err = sample.Time("greeting.delay")
-	if v != zero || err != ErrWrongType {
-		t.Fatalf("Config.Time(%q) -> %s, %#v (want %s, %#v)",
-			"greeting.delay", v, err, 0, ErrWrongType)
-	}
-
-	want := time.Date(2012, 12, 28, 15, 10, 15, 0, time.UTC)
-
-	v, err = sample.Time("timestamp")
-	if v != want || err != nil {
-		t.Fatalf("Config.Time(%q) -> %s, %#v (want %s, %#v)",
-			"timestamp", v, err, 8080, nil)
+	for _, test := range timeTests {
+		v, err := sample.Time(test.key)
+		if v != test.value || err != test.err {
+			t.Errorf("Config.Time(%q) -> %#v, %#v (want %#v, %#v)",
+				test.key, v, err, test.value, test.err)
+		}
 	}
 }
 
+var durationTests = []struct {
+	key   string
+	value time.Duration
+	err   error
+}{
+	{"undefined", 0, ErrUndefined},
+	{"string", 0, ErrWrongType},
+	{"bool", 0, ErrWrongType},
+	{"int64", 0, ErrWrongType},
+	{"float64", 0, ErrWrongType},
+	{"time", 0, ErrWrongType},
+	{"duration", 2 * time.Second, nil},
+}
+
 func TestConfigDuration(t *testing.T) {
-	v, err := sample.Duration("undefined")
-	if v != 0 || err != ErrUndefined {
-		t.Fatalf("Config.Duration(%q) -> %s, %#v (want %s, %#v)",
-			"undefined", v, err, 0, ErrUndefined)
-	}
-
-	v, err = sample.Duration("timestamp")
-	if v != 0 || err != ErrWrongType {
-		t.Fatalf("Config.Duration(%q) -> %s, %#v (want %s, %#v)",
-			"timestamp", v, err, 8080, nil)
-	}
-
-	v, err = sample.Duration("greeting.delay")
-	if v != 2*time.Second || err != nil {
-		t.Fatalf("Config.Duration(%q) -> %s, %#v (want %s, %#v)",
-			"greeting.delay", v, err, 0, ErrWrongType)
+	for _, test := range durationTests {
+		v, err := sample.Duration(test.key)
+		if v != test.value || err != test.err {
+			t.Errorf("Config.Duration(%q) -> %#v, %#v (want %#v, %#v)",
+				test.key, v, err, test.value, test.err)
+		}
 	}
 }
