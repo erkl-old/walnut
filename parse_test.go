6037e192dc631289aee9fcaf98be5ef8ebc957d7
Erik Lundin <erik@wub.io>
2013-01-30 11:40:50 +0000

    Extend parse_test.go

--- parse_test.go
+++ parse_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"reflect"
 	"testing"
+	"time"
 )
 
 var definitionTests = []struct {
@@ -65,6 +66,57 @@ var definitionTests = []struct {
 		nil,
 	},
 	{
+		"label\n  # empty\n key = \"test\"",
+		[]definition{
+			{"label.key", "test", "\"test\"", 3},
+		},
+		nil,
+	},
+	{
+		"bool = true",
+		[]definition{
+			{"bool", true, "true", 1},
+		},
+		nil,
+	},
+	{
+		"int64 = 12345",
+		[]definition{
+			{"int64", int64(12345), "12345", 1},
+		},
+		nil,
+	},
+	{
+		"float64 = 123.45",
+		[]definition{
+			{"float64", float64(123.45), "123.45", 1},
+		},
+		nil,
+	},
+	{
+		"string = \"hello\"",
+		[]definition{
+			{"string", "hello", "\"hello\"", 1},
+		},
+		nil,
+	},
+	{
+		"time = 2012-01-02 15:30:28.000000000789 +0000",
+		func() []definition {
+			raw := "2012-01-02 15:30:28.000000000789 +0000"
+			t, _ := time.Parse("2006-01-02 15:04:05 -0700", raw)
+			return []definition{{"time", t, raw, 1}}
+		}(),
+		nil,
+	},
+	{
+		"duration = 10m 20s",
+		[]definition{
+			{"duration", 10*time.Minute + 20*time.Second, "10m 20s", 1},
+		},
+		nil,
+	},
+	{
 		" foo=3",
 		nil,
 		errors.New("invalid indentation on line 1"),
@@ -88,6 +140,21 @@ var definitionTests = []struct {
 		"i\n\t j=7\n  k=8",
 		nil,
 		errors.New("invalid indentation on line 3"),
+	},
+	{
+		"= \"missing\"",
+		nil,
+		errors.New("invalid key on line 1"),
+	},
+	{
+		"invalid = 3 3",
+		nil,
+		errors.New("unrecognized value on line 1: \"3 3\""),
+	},
+	{
+		"invalid == 3",
+		nil,
+		errors.New("unrecognized value on line 1: \"= 3\""),
 	},
 }
 
